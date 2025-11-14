8af4a903524ff562d81f7a5f3f08ba84e7dd2171
Paul Rodriguez <paulyd0123@gmail.com>
2025-11-14 16:32:17 -0500

    created components folder and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <h1>Portfolio Coming Soon</h1>
     </div>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+
+function Navbar() {
+    return (
+        <div className="top-header">
+            <header>
+                <div className="name">Paul Rodriguez</div>
+                <nav className="navbar">
+                    <ul>
+                        <li><a href="#about-section">About</a></li>
+                        <li><a href="#projects-section">Projects</a></li>
+                        <li><a href="#skills-section">Skills</a></li>
+                        <li><a href="#contact-section">Leave a Message!</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar;
